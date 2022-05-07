ec828eca5640c281f9d9530aae94a00d4a475154
Vlad <vladislav.bok@yandex.ru>
2022-05-07 16:54:03 +0300

    Initial Commit

---
+++ SwiftUI test1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI test1
+//
+//  Created by Влад Бокин on 07.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI test1/SwiftUI_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_test1App.swift
+//  SwiftUI test1
+//
+//  Created by Влад Бокин on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
